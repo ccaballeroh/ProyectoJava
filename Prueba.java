f850c04f7de1f293cfa26a641508624aad60ae9a
Christian <cdch10@gmail.com>
2018-09-09 00:51:07 -0500

    Cambio al nombre de archivos de salida. Se generan todos de nuevo.

--- Prueba.java
+++ Prueba.java
@@ -4,39 +4,39 @@ public class Prueba {
   public static void main(String s[]){
     Grafo uno = new Grafo(358);
     uno.modeloER(237);
-    uno.escribirArchivo("Salidas\\GV\\Salida1.gv");
+    uno.escribirArchivo("Salidas\\GV\\Salida01.gv");
 
     Grafo dos = new Grafo(589);
     dos.modeloER(500);
-    uno.escribirArchivo("Salidas\\GV\\Salida2.gv");
+    dos.escribirArchivo("Salidas\\GV\\Salida02.gv");
 
     Grafo tres = new Grafo(1500);
     tres.modeloER(1289);
-    tres.escribirArchivo("Salidas\\GV\\Salida3.gv");
+    tres.escribirArchivo("Salidas\\GV\\Salida03.gv");
 
     Grafo cuatro = new Grafo(358);
     cuatro.modeloGilbert(1);
-    cuatro.escribirArchivo("Salidas\\GV\\Salida4.gv");
+    cuatro.escribirArchivo("Salidas\\GV\\Salida04.gv");
 
     Grafo cinco = new Grafo(589);
     cinco.modeloGilbert(0.3);
-    cinco.escribirArchivo("Salidas\\GV\\Salida5.gv");
+    cinco.escribirArchivo("Salidas\\GV\\Salida05.gv");
 
     Grafo seis = new Grafo(1500);
     seis.modeloGilbert(0.05);
-    seis.escribirArchivo("Salidas\\GV\\Salida6.gv");
+    seis.escribirArchivo("Salidas\\GV\\Salida06.gv");
 
     Grafo siete = new Grafo(358, "geo-uniforme");
     siete.modeloGeoSimple(1);
-    siete.escribirArchivo("Salidas\\GV\\Salida7.gv");
+    siete.escribirArchivo("Salidas\\GV\\Salida07.gv");
 
     Grafo ocho = new Grafo(589, "geo-uniforme");
     ocho.modeloGeoSimple(.5);
-    ocho.escribirArchivo("Salidas\\GV\\Salida8.gv");
+    ocho.escribirArchivo("Salidas\\GV\\Salida08.gv");
 
     Grafo nueve = new Grafo(1500, "geo-uniforme");
     nueve.modeloGeoSimple(0.1);
-    nueve.escribirArchivo("Salidas\\GV\\Salida9.gv");
+    nueve.escribirArchivo("Salidas\\GV\\Salida09.gv");
 
     Grafo diez = new Grafo(358);
     diez.modeloBA(30);
