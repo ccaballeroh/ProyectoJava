bb5081f7ae6d50c6d2e2d34b0628fda2016915c7
Christian <cdch10@gmail.com>
2018-09-15 04:23:08 -0500

    Se comenta la clase 'Grafo' y nombres de salidas en Prueba.java

--- Grafo.java
+++ Grafo.java
@@ -1,41 +1,59 @@
+/*Clase que implementa modelos de grafos.
+
+La clase forma parte del paquete 'Grafos'. Implementa cuatro modelos para
+genera grafos aleatorios:
+1. Erdös-Rényi
+2. Gilbert
+3. Geográfico simple
+4. Barabási-Albert
+
+La clase utiliza la clase 'Vertice' del mismo paquete, que incluye
+constructores para el modelo geográfico simple, que necesita un par de
+coordenadas, y otro para los otros tres modelos.*/
 package Grafos;
 import Grafos.Vertice;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Formatter;
-import java.util.NoSuchElementException;
-import java.util.FormatterClosedException;
+import java.util.*;
 import java.io.FileNotFoundException;
 
 public class Grafo
-{
+{ //Variables de instancia
+
+  /*arreglo de objetos 'Vertice'. De esta forma nos podemos referir a cada
+  vértice por su posición en el arreglo*/
   private Vertice[] nodes;
+  /*El grafo en sí es un mapa Hash. Toma como llave el vértice, que es mapeado
+  a un conjunto Hash de vértices con los cuales hay conexión.*/
   private HashMap<Vertice, HashSet<Vertice>> graph;
-  private int numeroVertices;
-  private int numeroAristas;
-  private static Formatter output;
+  private final int numeroVertices; //número de vértices del grafo
+  private int numeroAristas;  //número de aristas únicas del grafo
+  private static Formatter output; //objeto para escribir a disco
+
+
+  /* Constructores*/
 
   public Grafo(int numVertices)
   {
     this.graph = new HashMap<Vertice, HashSet<Vertice>>();
     this.numeroVertices = numVertices;
     this.nodes = new Vertice[numVertices];
-    for (int i = 0; i < numVertices; i++){
+    for (int i = 0; i < numVertices; i++) {
       Vertice n = new Vertice(i);
       this.nodes[i] = n;
       this.graph.put(n, new HashSet<Vertice>());
     }
   }
 
-  public Grafo(int numVertices, String modelo){
+  /*Constructor usado para el modelo geofráfico simple.
+
+  Se le tienen que pasar el número de vértices y la cadena "geo-uniforme"*/
+  public Grafo(int numVertices, String modelo) {
       this.graph = new HashMap<Vertice, HashSet<Vertice>>();
       this.numeroVertices = numVertices;
       this.nodes = new Vertice[numVertices];
       Random coorX = new Random();
       Random coorY = new Random();
-      if (modelo == "geo-uniforme"){
-        for (int i = 0; i < numVertices; i++){
+      if (modelo == "geo-uniforme") {
+        for (int i = 0; i < numVertices; i++) {
           Vertice n = new Vertice(i, coorX.nextDouble(), coorY.nextDouble());
           this.nodes[i] = n;
           this.graph.put(n, new HashSet<Vertice>());
@@ -43,98 +61,31 @@ public class Grafo
       }
     }
 
-  public void modeloER(int numAristas){
-    Random randomNum1 = new Random();
-    Random randomNum2 = new Random();
-
-    int conexiones = 0;
-    while (conexiones < numAristas){
-      int num1 = randomNum1.nextInt(this.getNumNodes());
-      int num2 = randomNum2.nextInt(this.getNumNodes());
-      if (num1 != num2){
-        if (!existeConexion(this.nodes[num1], this.nodes[num2])){
-          conectarVertices(this.nodes[num1], this.nodes[num2]);
-          conexiones++;
-        }
-      }
-    }
-  }
-
-  public void modeloGilbert(double probabilidad){
-    Random randomNum = new Random();
-
-    for(int i = 0; i < this.getNumNodes(); i++){
-      for(int j = 0; j <this.getNumNodes(); j++){
-        if ((i != j) && (randomNum.nextDouble() <= probabilidad)){
-          if (!existeConexion(this.nodes[i], this.nodes[j])){
-            conectarVertices(this.nodes[i], this.nodes[j]);
-          }
-        }
-      }
-    }
-  }
-
-  public void modeloGeoSimple(double r){
-    for(int i = 0; i < this.getNumNodes(); i++){
-      for(int j = 0; j < this.getNumNodes(); j++){
-        double distancia = distanciaVertices(this.nodes[i], this.nodes[j]);
-        if ((i != j) && (distancia <= r)){
-           if (!existeConexion(this.nodes[i], this.nodes[j])){
-            conectarVertices(this.nodes[i], this.nodes[j]);
-           }
-        }
-      }
-    }
-  }
-
-  public void modeloBA(int d){
-    Random volado = new Random();
-    for(int i = 0; i < this.getNumNodes();){
-      for(int j = 0; j < i; j++){
-        if (i < d){
-          if (!existeConexion(this.nodes[i], this.nodes[j])){
-            conectarVertices(this.nodes[i], this.nodes[j]);
-          }
-        }
-        else {
-          double probabilidad =
-          (double)gradoVertice(this.nodes[j])/(double)this.getNumEdges();
-          if (volado.nextDouble() <= probabilidad){
-            if ((!existeConexion(this.nodes[i], this.nodes[j])) && (gradoVertice(this.nodes[i]) < d)){
-              conectarVertices(this.nodes[i], this.nodes[j]);
-            }
-          }
-        }
-      }
-      if((i >= d) && (gradoVertice(this.nodes[i]) < d)){
-      //  *System.out.println("Vertice " + i + " y " + d + " y " + gradoVertice(this.nodes[i]) );
-      }
-      else if ((i >= d) && (gradoVertice(this.nodes[i]) >= d)){
-        i++;
-      }
-      else if (i < d){
-        i++;
-      }
-    }
-  }
-
-  private int gradoVertice(Vertice n1){
+  /*Métodos auxiliares*/
+  /*Regresa el grado (número de aristas) de un vértice*/
+  private int gradoVertice(Vertice n1) {
     return this.graph.get(n1).size();
   }
 
-  private void conectarVertices(Vertice n1, Vertice n2){
-     HashSet<Vertice> vertices1 = this.graph.get(n1);
-     HashSet<Vertice> vertices2 = this.graph.get(n2);
+  /*Conecta dos vértices*/
+  private void conectarVertices(Vertice n1, Vertice n2) {
+    /*Se recuperan las aristas de cada vértice*/
+     HashSet<Vertice> aristas1 = this.graph.get(n1);
+     HashSet<Vertice> aristas2 = this.graph.get(n2);
 
-     vertices1.add(n2);
-     vertices2.add(n1);  //en Grafos dirigidos hay que quitar esta
-     this.numeroAristas +=1;
+     /*Se agregan los vértices al conjunto del otro*/
+     aristas1.add(n2);
+     aristas2.add(n1);  //en Grafos dirigidos hay que quitar esta línea
+     this.numeroAristas +=1; //Para que sean aristas únicas (en lugar de 2)
   }
 
-  private Boolean existeConexion(Vertice n1, Vertice n2){
-    HashSet<Vertice> vertices1 = this.graph.get(n1);
-    HashSet<Vertice> vertices2 = this.graph.get(n2);
-     if (vertices1.contains(n2) || vertices2.contains(n1)){
+  /*Nos regresa 'true' si ya existe la arista*/
+  private Boolean existeConexion(Vertice n1, Vertice n2) {
+    /*Se recuperan las aristas de cada vértice*/
+    HashSet<Vertice> aristas1 = this.graph.get(n1);
+    HashSet<Vertice> aristas2 = this.graph.get(n2);
+    /*Se revisa que un nodo esté en el conjunto de aristas del otro*/
+     if (aristas1.contains(n2) || aristas2.contains(n1)) {
        return true;
      }
      else{
@@ -142,59 +93,154 @@ public class Grafo
      }
   }
 
-  private double distanciaVertices(Vertice n1, Vertice n2){
+  /*Distancia L2 entre vértices para el modelo geofráfico*/
+  private double distanciaVertices(Vertice n1, Vertice n2) {
     return Math.sqrt(Math.pow((n1.getX() - n2.getX()), 2)
     + Math.pow((n1.getY() - n2.getY()), 2));
   }
 
-  public int getNumNodes(){
-    return this.numeroVertices;
-  }
+  /*getters de las variables de instancia del objeto 'Grafo'*/
+  public int getNumNodes() {return numeroVertices;}
 
-  public int getNumEdges(){
-    return this.numeroAristas;
-  }
+  public int getNumEdges() {return numeroAristas;}
 
-  public void imprimirStdOut(){
-    System.out.println("graph {");
-    for (int i = 0; i < this.getNumNodes(); i++){
-      System.out.println(this.nodes[i].getName() + ";");
-    }
-    for (int i = 0; i < this.getNumNodes(); i++){
-      HashSet<Vertice> aristas = graph.get(this.nodes[i]);
-      for (Vertice n : aristas){
-      System.out.println(this.nodes[i].getName() + " -- " + n.getName() + ";");
+  /*Métodos de instancia que implementan los modelos*/
+
+  /*Modelo Erdös-Rényi.
+
+  Crea n vértices y elige uniformemente al azar m distintos
+   pares de distintos vértices*/
+  public void modeloER(int numAristas) {
+    Random randomNum1 = new Random();
+    Random randomNum2 = new Random();
+    /*mientras el número de aristas del grafo sea menor que el número con el
+    que se llamó este modelo se generan números al azar entre 0 y el número de
+    nodos -1. Se verifica si ya existe conexión entre los nodos que
+    corresponden a esos índices y si no, se conectan. Se verifica que
+    los números pseudoaleatorios no sean el mismo*/
+    while (this.getNumEdges() < numAristas) {
+      int num1 = randomNum1.nextInt(this.getNumNodes());
+      int num2 = randomNum2.nextInt(this.getNumNodes());
+      if (num1 != num2) {
+        if (!existeConexion(this.nodes[num1], this.nodes[num2])) {
+          conectarVertices(this.nodes[num1], this.nodes[num2]);
+        }
       }
-     }
-    System.out.println("}");
+    }
   }
 
-  public void escribirArchivo(String nombre){
+  /*Modelo de Gilbert.
+
+  Crear m aristas crear n vértices y poner una arista entre cada par
+  independiente y uniformemente con probabilidad p*/
+  public void modeloGilbert(double probabilidad) {
+    Random randomNum = new Random();
+    /*Para cada vértice i se recorren todos los vértices j con i<>j. En cada
+    par se calcula un número pseudoaleatorio entre 0.0 y 1.0 y se compara
+    con la probabilidad que se le pasó como argumento al modelo. Si es menor
+    que la probabilidad y no existe ya una conexión, se realiza la conexión.*/
+    for(int i = 0; i < this.getNumNodes(); i++) {
+      for(int j = 0; j <this.getNumNodes(); j++) {
+        if ((i != j) && (randomNum.nextDouble() <= probabilidad)) {
+          if (!existeConexion(this.nodes[i], this.nodes[j])) {
+            conectarVertices(this.nodes[i], this.nodes[j]);
+          }
+        }
+      }
+    }
+  }
+
+/*Modelo geográfico simple.
+
+Colocar n vértices en un rectángulo unitario con coordenadas uniformes
+(o normales) y colocar una arista entre cada par que queda a una
+distancia r o menor*/
+  public void modeloGeoSimple(double r) {
+    /*Para cada vértice i se compara con el resto de los vértices con los que
+    no se ha comparado y si están a una distancia r o menor se realiza la
+    conexión.*/
+    for(int i = 0; i < this.getNumNodes(); i++) {
+      for(int j = i + 1; j < this.getNumNodes(); j++) {
+        double distancia = distanciaVertices(this.nodes[i], this.nodes[j]);
+        if (distancia <= r) {
+            conectarVertices(this.nodes[i], this.nodes[j]);
+        }
+      }
+    }
+  }
+
+
+  /*Modelo Barabási-Albert.
+
+  Colocar n vértices uno por uno, asignando a cada uno d aristas a vértices
+  distintos de tal manera que la probabilidad de que el vértice nuevo se
+  conecte a un vértice existente v es proporcional a la cantidad de aristas que
+  v tiene actualmente - los primeros d vértices se conecta todos a todos*/
+  public void modeloBA(int d) {
+    Random volado = new Random();
+  /*Para los primeros d vértices, se conecta el vértice i con el vértice j
+  con i distinto de j y recorriendo todos los vértices.*/
+    for(int i = 0; i < d; i++){
+      for(int j = 0; j < i; j++) {
+        if (!existeConexion(this.nodes[i], this.nodes[j])) {
+          conectarVertices(this.nodes[i], this.nodes[j]);
+        }
+      }
+    }
+  /*Del vértice d en adelante hasta el vértice n, el vértice i de trata de
+  emparejar con cada vértice j desde el primero hasta i-1. La manera de hacer
+  el emparejamiento es con probabilidad. La probabilidad de que el vértice i
+  se conecte con j es proporcional al número de aristas del vértice j dividido
+  por el número total de aristas en el grafo hasta ese momento. Un número
+  pseudoaleatorio se compara con esa probabilidad, de ser menor, se realiza
+  la conexión. Antes de realizar la conexión se verifica que no exista ya y que
+  el vértice i no tenga ya d o más conexiones.*/
+  // i no se incrementa hasta que ese vértice tiene d conexiones
+    for(int i = d; i < this.getNumNodes();) {
+      for(int j = 0; j < i; j++) {
+        double probabilidad =
+        (double)gradoVertice(this.nodes[j])/(double)this.getNumEdges();
+        if (volado.nextDouble() <= probabilidad) {
+          if ((!existeConexion(this.nodes[i], this.nodes[j]))
+                && (gradoVertice(this.nodes[i]) < d)){
+            conectarVertices(this.nodes[i], this.nodes[j]);
+          }
+        }
+      }
+      if (gradoVertice(this.nodes[i]) >= d) i++;
+    }
+  }
+
+/*Método para escribir a disco en un formato GraphVis.
+
+El método toma como argumento, el nombre del archivo.*/
+  public void escribirArchivo(String nombre) {
     try{
       output = new Formatter(nombre);
     }
-    catch (SecurityException securityException){
+    catch (SecurityException securityException) {
       System.err.println("No hay permiso de escritura.");
       System.exit(1);
     }
-    catch (FileNotFoundException fileNotFoundException){
+    catch (FileNotFoundException fileNotFoundException) {
       System.err.println("Error al abrir el archivo.");
       System.exit(1);
     }
     try{
       output.format("%s","graph {\n");
-      for (int i = 0; i < this.getNumNodes(); i++){
+      for (int i = 0; i < this.getNumNodes(); i++) {
         output.format("%s", this.nodes[i].getName() + ";\n");
       }
-      for (int i = 0; i < this.getNumNodes(); i++){
+      for (int i = 0; i < this.getNumNodes(); i++) {
         HashSet<Vertice> aristas = graph.get(this.nodes[i]);
-        for (Vertice n : aristas){
-        output.format("%s",this.nodes[i].getName() + " -- " + n.getName() + ";\n");
+        for (Vertice n : aristas) {
+        output.format("%s",this.nodes[i].getName() + " -- "
+                      + n.getName() + ";\n");
         }
        }
       output.format("}\n");
     }
-    catch (FormatterClosedException formatterClosedException){
+    catch (FormatterClosedException formatterClosedException) {
       System.err.println("Error al escribir al archivo");
     }
     if (output != null)
--- Prueba.java
+++ Prueba.java
@@ -3,51 +3,51 @@ import Grafos.Grafo;
 public class Prueba {
   public static void main(String s[]){
     Grafo uno = new Grafo(30);
-    uno.modeloER(15);
-    uno.escribirArchivo("Salidas\\GV\\Salida01.gv");
+    uno.modeloER(8);
+    uno.escribirArchivo("Salidas\\GV\\01ER030.gv");
 
     Grafo dos = new Grafo(100);
-    dos.modeloER(73);
-    dos.escribirArchivo("Salidas\\GV\\Salida02.gv");
+    dos.modeloER(25);
+    dos.escribirArchivo("Salidas\\GV\\01ER100.gv");
 
     Grafo tres = new Grafo(500);
-    tres.modeloER(380);
-    tres.escribirArchivo("Salidas\\GV\\Salida03.gv");
+    tres.modeloER(75);
+    tres.escribirArchivo("Salidas\\GV\\01ER500.gv");
 
     Grafo cuatro = new Grafo(30);
-    cuatro.modeloGilbert(1);
-    cuatro.escribirArchivo("Salidas\\GV\\Salida04.gv");
+    cuatro.modeloGilbert(0.25);
+    cuatro.escribirArchivo("Salidas\\GV\\02Gilbert030.gv");
 
     Grafo cinco = new Grafo(100);
-    cinco.modeloGilbert(0.3);
-    cinco.escribirArchivo("Salidas\\GV\\Salida05.gv");
+    cinco.modeloGilbert(0.25);
+    cinco.escribirArchivo("Salidas\\GV\\02Gilbert100.gv");
 
     Grafo seis = new Grafo(500);
-    seis.modeloGilbert(0.05);
-    seis.escribirArchivo("Salidas\\GV\\Salida06.gv");
+    seis.modeloGilbert(0.25);
+    seis.escribirArchivo("Salidas\\GV\\02Gilbert500.gv");
 
     Grafo siete = new Grafo(30, "geo-uniforme");
-    siete.modeloGeoSimple(0.9);
-    siete.escribirArchivo("Salidas\\GV\\Salida07.gv");
+    siete.modeloGeoSimple(0.3);
+    siete.escribirArchivo("Salidas\\GV\\03Geo030.gv");
 
     Grafo ocho = new Grafo(100, "geo-uniforme");
-    ocho.modeloGeoSimple(.5);
-    ocho.escribirArchivo("Salidas\\GV\\Salida08.gv");
+    ocho.modeloGeoSimple(.3);
+    ocho.escribirArchivo("Salidas\\GV\\03Geo100.gv");
 
     Grafo nueve = new Grafo(500, "geo-uniforme");
-    nueve.modeloGeoSimple(0.1);
-    nueve.escribirArchivo("Salidas\\GV\\Salida09.gv");
+    nueve.modeloGeoSimple(0.3);
+    nueve.escribirArchivo("Salidas\\GV\\03Geo500.gv");
 
     Grafo diez = new Grafo(30);
-    diez.modeloBA(20);
-    diez.escribirArchivo("Salidas\\GV\\Salida10.gv");
+    diez.modeloBA(7);
+    diez.escribirArchivo("Salidas\\GV\\04BA030.gv");
 
     Grafo once = new Grafo(100);
-    once.modeloBA(73);
-    once.escribirArchivo("Salidas\\GV\\Salida11.gv");
+    once.modeloBA(25);
+    once.escribirArchivo("Salidas\\GV\\04BA100.gv");
 
     Grafo doce = new Grafo(500);
-    doce.modeloBA(135);
-    doce.escribirArchivo("Salidas\\GV\\Salida12.gv");
+    doce.modeloBA(100);
+    doce.escribirArchivo("Salidas\\GV\\04BA500.gv");
   }
 }
