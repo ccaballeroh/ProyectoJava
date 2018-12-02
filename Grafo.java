55d61073f7a8dca8f3d0c95d8f5f7917b5fa1ab6
Christian <cdch10@gmail.com>
2018-12-01 19:28:32 -0600

    Se arreglan comentarios

--- Grafo.java
+++ Grafo.java
@@ -12,11 +12,12 @@ constructores para el modelo geográfico simple, que necesita un par de
 coordenadas, y otro para los otros tres modelos.*/
 package Grafos;
 import Grafos.Vertice;
+import Grafos.Arista;
 import java.util.*;
 import java.io.FileNotFoundException;
 
-public class Grafo
-{ //Variables de instancia
+public class Grafo {
+  //////////Variables de instancia//////////
 
   /*arreglo de objetos 'Vertice'. De esta forma nos podemos referir a cada
   vértice por su posición en el arreglo*/
@@ -24,18 +25,16 @@ public class Grafo
   /*El grafo en sí es un mapa Hash. Toma como llave el vértice, que es mapeado
   a un conjunto Hash de vértices con los cuales hay conexión.*/
   private HashMap<Vertice, HashSet<Vertice>> graph;
-  private HashMap<Vertice, HashSet<Arista>> incidencia;
+  private HashMap<Vertice, HashSet<Arista>> incidencia; //mapa para Dijkstra
   private final int numeroVertices; //número de vértices del grafo
   private int numeroAristas;  //número de aristas únicas del grafo
   private static Formatter output; //objeto para escribir a disco
+  private Boolean weighted; //bandera a usar si grafo es pesado
 
-  private Boolean weighted;
 
+  //////////Constructores//////////
 
-  /* Constructores*/
-
-  public Grafo(int numVertices)
-  {
+  public Grafo(int numVertices) {
     this.graph = new HashMap<Vertice, HashSet<Vertice>>();
     this.numeroVertices = numVertices;
     this.nodes = new Vertice[numVertices];
@@ -48,7 +47,6 @@ public class Grafo
   }
 
   /*Constructor usado para el modelo geofráfico simple.
-
   Se le tienen que pasar el número de vértices y la cadena "geo-uniforme"*/
   public Grafo(int numVertices, String modelo) {
       this.graph = new HashMap<Vertice, HashSet<Vertice>>();
@@ -66,14 +64,14 @@ public class Grafo
       this.weighted = false;
     }
 
-  /*Métodos auxiliares*/
-  /*Regresa el grado (número de aristas) de un vértice i*/
+  //////////Métodos auxiliares//////////
+  //Regresa el grado (número de aristas) de un vértice i
   public int gradoVertice(int i) {
     Vertice n1 = this.getNode(i);
     return this.graph.get(n1).size();
   }
 
-  /*Conecta dos vértices*/
+  //Conecta dos vértices
   public void conectarVertices(int i, int j) {
     /*Se recuperan los vértices de los índices i y j*/
      Vertice n1 = this.getNode(i);
@@ -88,7 +86,7 @@ public class Grafo
      this.numeroAristas +=1; //Para que sean aristas únicas (en lugar de 2)
   }
 
-  /*Nos regresa 'true' si ya existe la arista*/
+  //Regresa 'true' si ya existe la arista
   private Boolean existeConexion(int i, int j) {
     /*Se recuperan los vértices de los índices i y j*/
      Vertice n1 = this.getNode(i);
@@ -105,13 +103,13 @@ public class Grafo
      }
   }
 
-  /*Distancia L2 entre vértices para el modelo geofráfico*/
+  //Distancia L2 entre vértices para el modelo geofráfico
   private double distanciaVertices(Vertice n1, Vertice n2) {
     return Math.sqrt(Math.pow((n1.getX() - n2.getX()), 2)
     + Math.pow((n1.getY() - n2.getY()), 2));
   }
 
-  /*getters de las variables de instancia del objeto 'Grafo'*/
+  //////////getters de las variables de instancia//////////
   public int getNumNodes() {return numeroVertices;}
 
   public int getNumEdges() {return numeroAristas;}
@@ -130,7 +128,7 @@ public class Grafo
     return this.incidencia.get(n);
   }
 
-  /*Método toString para tener representación en String del Grafo*/
+  //////////Método toString para representación en String del Grafo//////////
   public String toString() {
     String salida;
     if (this.getWeightedFlag()) {
@@ -162,7 +160,7 @@ public class Grafo
     return salida;
   }
 
-  /*Métodos de instancia que implementan los modelos*/
+  //////////Métodos de instancia de los modelos de generación//////////
 
   /*Modelo Erdös-Rényi.
 
@@ -208,7 +206,7 @@ public class Grafo
     }
   }
 
-/*Modelo geográfico simple.
+  /*Modelo geográfico simple.
 
 Colocar n vértices en un rectángulo unitario con coordenadas uniformes
 (o normales) y colocar una arista entre cada par que queda a una
@@ -226,7 +224,6 @@ distancia r o menor*/
       }
     }
   }
-
 
   /*Modelo Barabási-Albert.
 
@@ -268,9 +265,9 @@ distancia r o menor*/
     }
   }
 
-/*Método para escribir a disco en un formato GraphVis.
+  /*Método para escribir a disco en un formato GraphVis.
 
-El método toma como argumento, el nombre del archivo.*/
+  El método toma como argumento, el nombre del archivo.*/
   public void escribirArchivo(String nombre) {
     try{
       output = new Formatter(nombre);
@@ -293,6 +290,7 @@ El método toma como argumento, el nombre del archivo.*/
     output.close();
   }
 
+  ////// SEGUNDA ENTREGA //////////
   /* Método para generar el árbol BFS del Grafo */
   /* Regresa otro grafo. Solo toma como entrada el número
   de un nodo*/
@@ -320,12 +318,12 @@ El método toma como argumento, el nombre del archivo.*/
         }
       }
     }
-return arbol;
+    return arbol;
   }
 
   /* Método para generar el árbol DFS del Grafo de forma recursiva  */
   /* Regresa otro grafo. Solo toma como entrada el número de un nodo*/
-public Grafo DFS_R(int s) {
+  public Grafo DFS_R(int s) {
   Grafo arbol = new Grafo(this.getNumNodes());  // grafo de salida
   Boolean[] discovered = new Boolean[this.getNumNodes()];  // arreglo aux
   for (int i = 0; i < this.getNumNodes(); i++) {
@@ -336,7 +334,7 @@ public Grafo DFS_R(int s) {
   return arbol;
 }
 
-private void recursivoDFS(int u, Boolean[] discovered, Grafo arbol) {
+  private void recursivoDFS(int u, Boolean[] discovered, Grafo arbol) {
   discovered[u] = true;  // vértice con el que se llamó se marca
   // aristas del vértice u, con el que se llamó el método
   HashSet<Vertice> aristas = this.getEdges(u);
@@ -351,8 +349,8 @@ private void recursivoDFS(int u, Boolean[] discovered, Grafo arbol) {
     }
 
   /* Método para generar el árbol DFS del Grafo de forma iterativa  */
-  /* Regresa otro grafo. Solo toma como entrada el número de un nodo*/
-public Grafo DFS_I(int s) {
+/* Regresa otro grafo. Solo toma como entrada el número de un nodo*/
+  public Grafo DFS_I(int s) {
   Grafo arbol = new Grafo(this.getNumNodes());  // grafo de salida
   Boolean[] explored = new Boolean[this.getNumNodes()];  // arreglo aux
   Stack<Integer> S = new Stack<Integer>(); //pila para los vértices
@@ -379,11 +377,14 @@ public Grafo DFS_I(int s) {
   return arbol;
   }
 
+
+  ////////// TERCERA ENTREGA //////////
+
+  ///////// Método para asignar pesos a las aristas entre dos valores /////////
   public void EdgeValues(double min, double max) {
     Random rand = new Random();
     double peso;
     this.incidencia = new HashMap<>();
-    //HashMap<Integer, HashSet<Arista>>  incidencia = new HashMap<>();
     for(int i = 0; i < this.getNumNodes(); i++) {
       HashSet<Vertice> aristasNodo = this.getEdges(i);
       HashSet<Arista> aristasPeso = new HashSet<>();
